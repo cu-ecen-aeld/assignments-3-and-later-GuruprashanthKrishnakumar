a0a90689f5f44d804c823eb48936bfb22aa90577
GuruprashanthKrishnakumar <gukr5411@colorado.edu>
2022-08-25 15:30:47 -0600

    First Commit

---
+++ finder-app/writer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ $# -lt 2 ]
+then
+    echo Please supply the directory name as arg1 and the string to search as arg2
+    exit 1
+fi
+echo $2 > $1
+if [ $? -eq 1 ] 
+then
+    echo File could not be created
+    exit 1
+else
+    echo File created and string written
+fi
